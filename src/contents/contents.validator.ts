46d2059a6e74b489677dd25b5bf110e4e8e471a9
Presab <presabkhadka30@gmail.com>
2025-08-05 23:58:33 +0545

    feat: content validation with pipes

--- src/contents/contents.validator.ts
+++ src/contents/contents.validator.ts
@@ -1,4 +1,12 @@
 import * as z from 'zod';
+import { ContentsService } from './contents.service';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  PipeTransform,
+} from '@nestjs/common';
 
 const types = ['BLOG', 'ARTICLE', 'RANT'];
 
@@ -7,6 +15,37 @@ export const createContentValidator = z.object({
   type: z.enum(types),
   authorId: z.string().min(1, 'author id must be provided').optional(),
 });
+
+@Injectable()
+export class CreateContentValidaitonPipe implements PipeTransform {
+  constructor(private readonly contentsService: ContentsService) {}
+
+  async transform(value: any, metadata: ArgumentMetadata) {
+    try {
+      const validatedData = createContentValidator.parse(value);
+
+      const existingContent = await this.contentsService.findOne(
+        validatedData.name,
+      );
+
+      if (existingContent) {
+        throw new BadRequestException(
+          'There already exists a content with this id',
+        );
+      }
+
+      return validatedData;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new InternalServerErrorException(error.message);
+      } else {
+        throw new InternalServerErrorException(
+          'Somethign went wrong with the server',
+        );
+      }
+    }
+  }
+}
 
 export const updateContentValidator = z.object({
   name: z.string().min(1, 'Content name must be provided').optional(),

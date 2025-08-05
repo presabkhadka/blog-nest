9ef09bf727675db850bf9e6b5d037f2ef280135d
Presab <presabkhadka30@gmail.com>
2025-08-05 23:29:39 +0545

    feat: content crud service

--- src/contents/contents.service.ts
+++ src/contents/contents.service.ts
@@ -1,24 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
+const prisma = new PrismaClient();
 
 @Injectable()
 export class ContentsService {
-  create() {
-    return 'This action adds a new content';
+  async create(data: any) {
+    return await prisma.content.create({
+      ...data,
+    });
   }
 
-  findAll() {
-    return `This action returns all contents`;
+  async findAll() {
+    return await prisma.content.findMany({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} content`;
+  async findOne(id: number) {
+    return await prisma.content.findFirst({
+      where: {
+        id: id,
+      },
+    });
   }
 
-  update(id: number) {
-    return `This action updates a #${id} content`;
+  async update(id: number, data: { name?: string; type?: any }) {
+    return await prisma.content.update({
+      where: {
+        id: id,
+      },
+      data: {
+        ...data,
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} content`;
+  async remove(id: number) {
+    return await prisma.content.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
